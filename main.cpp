c003bc08802aa7650e361e5da4a7c2c9f82cc1bd
hassanbabar78 <130081518+hassanbabar78@users.noreply.github.com>
2023-11-29 22:07:32 +0500

    Add files via upload
    Hello g DOS sahab I am back

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#include<stdexcept>
+#include<vector>
+#include<list>
+#include<string>
+#include<map>
+#include<queue>
+#include"Prompt.h"
+
+
+int main()
+{
+	prompt P;
+	P.run();
+	return 0;
+}
